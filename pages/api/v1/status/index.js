a1056406662aac440fb26a1dbe5d155819ea4524
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2024-09-17 14:20:55 -0300

    chore: adds eslint job in linting github action

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  let variavelEsquecida;
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
