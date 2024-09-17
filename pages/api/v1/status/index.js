0eeb5d35cba19fb6e93d30b7ea7dac760ba663bb
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2024-09-17 14:27:03 -0300

    fix: eslint linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,6 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  let variavelEsquecida;
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
